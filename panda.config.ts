4dfdb2aae9d17887ad8fa87f325087b4e3f396bc
nyatinte <takumi.062757@gmail.com>
2024-04-14 19:07:26 +0900

    Configure park-ui

--- panda.config.ts
+++ panda.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from '@pandacss/dev'
+import { createPreset } from '@park-ui/panda-preset'
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
-
+  presets: ['@pandacss/preset-base', createPreset({ borderRadius: 'lg' })],
   // Where to look for your css declarations
   include: [
-    './app/routes/**/*.{ts,tsx,js,jsx}',
-    './app/components/**/*.{ts,tsx,js,jsx}',
+    './app/routes/**/*.{js,jsx,ts,tsx}',
+    './app/components/**/*.{js,jsx,ts,tsx}',
   ],
-
   // Files to exclude
   exclude: [],
-
+  jsxFramework: 'react',
   // The output directory for your css system
   outdir: 'styled-system',
 })
