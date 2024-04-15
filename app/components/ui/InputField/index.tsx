c3405b99104dfe4e5cd75f68bac655a5a1c1636d
nyatinte <takumi.062757@gmail.com>
2024-04-15 23:53:22 +0900

    Implement query2doc logic without OpenAI

--- app/components/ui/InputField/index.tsx
+++ app/components/ui/InputField/index.tsx
@@ -2,15 +2,17 @@ import { type ReactNode, forwardRef } from 'react'
 import { Flex, Stack } from 'styled-system/jsx'
 import { FormLabel } from '../FormLabel'
 import { Input, type InputProps } from '../Input'
+import { Text } from '../Text'
 
 type InputFieldProps = InputProps & {
   id: string
   label: string
   labelIcon?: ReactNode
+  error?: string
 }
 export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
   (props, ref) => {
-    const { label, labelIcon, id, ...inputProps } = props
+    const { label, labelIcon, id, error, ...inputProps } = props
 
     return (
       <Stack>
@@ -19,6 +21,7 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
           {labelIcon}
         </Flex>
         <Input id={id} ref={ref} {...inputProps} />
+        {error && <Text color={'red'}>{error}</Text>}
       </Stack>
     )
   },
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import type { MetaFunction } from '@remix-run/cloudflare'
 import { Link as RemixLink } from '@remix-run/react'
 import {
@@ -6,17 +7,20 @@ import {
   ExternalLinkIcon,
   XIcon,
 } from 'lucide-react'
+import { useState } from 'react'
+import { type SubmitHandler, useForm, useWatch } from 'react-hook-form'
 import { css } from 'styled-system/css'
 import { Box, Container, Flex, Stack } from 'styled-system/jsx'
 import { center, container } from 'styled-system/patterns'
+import { z } from 'zod'
 import { Badge } from '~/components/ui/Badge'
 import { Button } from '~/components/ui/Button'
 import * as Dialog from '~/components/ui/Dialog'
 import { Heading } from '~/components/ui/Heading'
-import { Icon } from '~/components/ui/Icon'
 import { IconButton } from '~/components/ui/IconButton'
 import { InputField } from '~/components/ui/InputField'
 import { Link } from '~/components/ui/Link'
+import { Skeleton } from '~/components/ui/Skeleton'
 import * as Tabs from '~/components/ui/Tabs'
 import { Text } from '~/components/ui/Text'
 
@@ -27,11 +31,41 @@ export const meta: MetaFunction = () => {
   ]
 }
 
+const schema = z.object({
+  apiKey: z.string().min(1, 'API Key is required'),
+  query: z.string().min(1, 'Query is required'),
+})
+
+type FormValues = z.infer<typeof schema>
+
 export default function Index() {
   const tabOptions = [
     { id: 'dense', label: 'Dense' },
     { id: 'sparse', label: 'Sparse' },
   ] as const satisfies readonly { id: string; label: string }[]
+
+  const [pseudoDocument, setPseudoDocument] = useState<string>()
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<FormValues> = async (values) => {
+    console.log(values)
+    const response: string = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('hogeohge')
+      }, 1000)
+    })
+
+    setPseudoDocument(response)
+  }
+
+  const query = useWatch<FormValues>({ control, name: 'query' })
 
   return (
     <Container className={css({ paddingY: '4' })}>
@@ -45,61 +79,77 @@ export default function Index() {
         >
           Query2Doc Demo
         </Heading>
-        <Stack gap="4" className={container({ minW: '3xl' })}>
-          <InputField
-            id="api-key"
-            label="Open AI API KEY"
-            type="password"
-            labelIcon={
-              <Dialog.Root>
-                <Dialog.Trigger>
-                  <Icon className={center()}>
-                    <CircleHelpIcon />
-                  </Icon>
-                </Dialog.Trigger>
-                <Dialog.Backdrop />
-                <Dialog.Positioner>
-                  <Dialog.Content p={4} maxW={'xl'}>
-                    <Dialog.Title>API KEY</Dialog.Title>
-                    <Dialog.Description>
-                      <Text>
-                        You need an API key to use this service. The API key is
-                        used exclusively for this service, processed on the
-                        backend, and not stored in any database.
-                      </Text>
-                      <Link mt={4} asChild>
-                        <RemixLink to="https://openai.com/">
-                          Get your API Key
-                          <ExternalLinkIcon />
-                        </RemixLink>
-                      </Link>
-                    </Dialog.Description>
-                    <Dialog.CloseTrigger
-                      asChild
-                      position="absolute"
-                      top="2"
-                      right="2"
-                    >
-                      <IconButton
-                        aria-label="Close Dialog"
-                        variant="ghost"
-                        size="sm"
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack gap="4" className={container({ minW: '3xl' })}>
+            <InputField
+              id="api-key"
+              label="Open AI API KEY"
+              type="password"
+              error={errors.apiKey?.message}
+              readOnly={isSubmitting || !!pseudoDocument}
+              {...register('apiKey')}
+              labelIcon={
+                <Dialog.Root>
+                  <Dialog.Trigger asChild>
+                    <IconButton variant={'ghost'} className={center()}>
+                      <CircleHelpIcon
+                        className={css({
+                          cursor: 'pointer',
+                        })}
+                      />
+                    </IconButton>
+                  </Dialog.Trigger>
+                  <Dialog.Backdrop />
+                  <Dialog.Positioner>
+                    <Dialog.Content p={4} maxW={'xl'}>
+                      <Dialog.Title>API KEY</Dialog.Title>
+                      <Dialog.Description>
+                        <Text>
+                          You need an API key to use this service. The API key
+                          is used exclusively for this service, and not stored
+                          in any database.
+                        </Text>
+                        <Link mt={4} asChild>
+                          <RemixLink to="https://openai.com/">
+                            Get your API Key
+                            <ExternalLinkIcon />
+                          </RemixLink>
+                        </Link>
+                      </Dialog.Description>
+                      <Dialog.CloseTrigger
+                        asChild
+                        position="absolute"
+                        top="2"
+                        right="2"
                       >
-                        <XIcon />
-                      </IconButton>
-                    </Dialog.CloseTrigger>
-                  </Dialog.Content>
-                </Dialog.Positioner>
-              </Dialog.Root>
-            }
-          />
-          <Flex gap="4" w="full" alignItems="end">
-            <Box flex="1">
-              <InputField id="query" label="Query" />
-            </Box>
-            <Button>Expand Query</Button>
-          </Flex>
-        </Stack>
+                        <IconButton
+                          aria-label="Close Dialog"
+                          variant="ghost"
+                          size="sm"
+                        >
+                          <XIcon />
+                        </IconButton>
+                      </Dialog.CloseTrigger>
+                    </Dialog.Content>
+                  </Dialog.Positioner>
+                </Dialog.Root>
+              }
+            />
+            <Flex gap="4" w="full" alignItems="end">
+              <Box flex="1">
+                <InputField
+                  id="query"
+                  label="Query"
+                  error={errors.query?.message}
+                  spellCheck
+                  readOnly={isSubmitting || !!pseudoDocument}
+                  {...register('query')}
+                />
+              </Box>
+              <Button type="submit">Expand Query</Button>
+            </Flex>
+          </Stack>
+        </form>
         <Flex gap="4" alignItems="center">
           <div
             className={css({
@@ -115,7 +165,16 @@ export default function Index() {
             <Badge mb="4" variant="outline">
               Pseudo Document
             </Badge>
-            <p>hogehoge</p>
+            {isSubmitting ? (
+              <Stack gap="3.5" w="full" animation="fade-in 0.5s ease-in-out">
+                {Array.from({ length: 5 }).map((_, i) => (
+                  // biome-ignore lint/suspicious/noArrayIndexKey: order is not important
+                  <Skeleton key={i} h="4" />
+                ))}
+              </Stack>
+            ) : (
+              <Text>{pseudoDocument}</Text>
+            )}
           </div>
           <ArrowRightIcon size="32" />
           <div
@@ -135,12 +194,19 @@ export default function Index() {
             <Tabs.Root variant="enclosed" defaultValue="dense">
               <Tabs.List>
                 {tabOptions.map((option) => (
-                  <Tabs.Trigger value={option.id}>{option.label}</Tabs.Trigger>
+                  <Tabs.Trigger key={option.id} value={option.id}>
+                    {option.label}
+                  </Tabs.Trigger>
                 ))}
                 <Tabs.Indicator />
               </Tabs.List>
-              <Tabs.Content value="dense">dense</Tabs.Content>
-              <Tabs.Content value="sparse">sparse</Tabs.Content>
+              <Tabs.Content value="dense">
+                {pseudoDocument && `${query} [SEP] ${pseudoDocument}`}
+              </Tabs.Content>
+              <Tabs.Content value="sparse">
+                {pseudoDocument &&
+                  `${Array(5).fill(query).join(' ')} ${pseudoDocument}`}
+              </Tabs.Content>
             </Tabs.Root>
           </div>
         </Flex>
